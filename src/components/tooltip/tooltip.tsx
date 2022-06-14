40d8ea7fe655256b13041a440f0f88cf1c84de3e
Julia Casamitjana <julia.casamitjana@hpi.de>
2022-06-14 17:21:13 +0200

    Improve tooltip component - It gives the tooltip component the ability to render an image when passed as props - It also prevents the tooltip to overflow its container

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -12,19 +12,35 @@ export class Tooltip {
 
   @Prop() content: string;
 
+  @Prop() image?: string;
+
   // Optional props. Default left position is 50%.
   @Prop() positionX?: number;
 
   componentDidUpdate() {
     if (this.positionX) {
-      this.el.style.left = `${this.positionX - this.el.clientWidth * 0.5}px`;
+      const centeredPosition = this.positionX - this.el.clientWidth * 0.5;
+      const containerWidth = this.el.parentElement.clientWidth;
+
+      const overflowsToTheLeft = centeredPosition <= 0;
+      const overflowsToTheRight =
+        this.positionX + this.el.clientWidth * 0.5 >= containerWidth;
+
+      if (overflowsToTheLeft) {
+        this.el.style.left = `0px`;
+      } else if (overflowsToTheRight) {
+        this.el.style.left = `${containerWidth - this.el.clientWidth}px`;
+      } else {
+        this.el.style.left = `${centeredPosition}px`;
+      }
     }
   }
 
   public render() {
     return (
       <div class="tooltip" role="tooltip" hidden={!this.show}>
-        <p>{this.content}</p>
+        {this.image && <img class="tooltip__image" src={this.image}></img>}
+        <p class="tooltip__content">{this.content}</p>
       </div>
     );
   }
