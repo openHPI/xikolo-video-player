e1f09c953aa0cc9fc992eefe7f555c2a2eafb33e
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-09-29 16:01:02 +0200

    Fix TS error

--- src/utils/testing-helpers.ts
+++ src/utils/testing-helpers.ts
@@ -14,43 +14,37 @@ import { E2EPage } from '@stencil/core/testing';
 // https://github.com/Esri/calcite-components/pull/1103
 
 export async function getSettingsMenuElement(page: E2EPage, selector: string) {
-  return (
-    await page.waitForFunction(
-      (shadowDomSelector) =>
-        document
-          .querySelector('xm-player')
-          ?.shadowRoot?.querySelector('xm-controls')
-          ?.shadowRoot?.querySelector('xm-settings-menu')
-          ?.shadowRoot?.querySelector(shadowDomSelector[0]),
-      {},
-      [selector],
-    )
-  ).asElement()!;
+  return (await page.waitForFunction(
+    (shadowDomSelector) =>
+      document
+        .querySelector('xm-player')
+        ?.shadowRoot?.querySelector('xm-controls')
+        ?.shadowRoot?.querySelector('xm-settings-menu')
+        ?.shadowRoot?.querySelector(shadowDomSelector[0]),
+    {},
+    [selector],
+  ))!;
 }
 
 export async function getControlsElement(page: E2EPage, selector: string) {
-  return (
-    await page.waitForFunction(
-      (shadowDomSelector) =>
-        document
-          .querySelector('xm-player')
-          ?.shadowRoot?.querySelector('xm-controls')
-          ?.shadowRoot?.querySelector(shadowDomSelector[0]),
-      {},
-      [selector],
-    )
-  ).asElement()!;
+  return (await page.waitForFunction(
+    (shadowDomSelector) =>
+      document
+        .querySelector('xm-player')
+        ?.shadowRoot?.querySelector('xm-controls')
+        ?.shadowRoot?.querySelector(shadowDomSelector[0]),
+    {},
+    [selector],
+  ))!;
 }
 
 export const getSettingsMenu = async (page: E2EPage) => {
-  const settingsMenu = (
-    await page.waitForFunction(
-      () =>
-        document
-          .querySelector('xm-player')
-          ?.shadowRoot?.querySelector('xm-controls')
-          ?.shadowRoot?.querySelector('xm-settings-menu'),
-    )
-  ).asElement()!;
+  const settingsMenu = (await page.waitForFunction(
+    () =>
+      document
+        .querySelector('xm-player')
+        ?.shadowRoot?.querySelector('xm-controls')
+        ?.shadowRoot?.querySelector('xm-settings-menu'),
+  ))!;
   return settingsMenu;
 };
